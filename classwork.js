3a9767d2578cd25c82cbcd6bc8a801b1bbba38ac
Mohsin-Israr1 <mohsinisrarpgc@gmail.com>
2022-06-28 01:01:36 -0700

    2nd day

---
+++ classwork.js
@@ -0,0 +1,23 @@
+// if(true) {
+//     let name = "Mustansar";
+//     if (true){
+//         console.log(name);
+//     }
+// }
+// const Value=null;
+// console.log(typeof myNullValue)
+
+
+// const obj={
+//     name: "mustansar",
+//     class: "game",
+//     obj["score"]=3.2:
+// }
+// console.log(obj);
+
+// if ({})
+// {
+// console.log("running");
+// }
+
+
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-let n = 5;
-let string = "";
+// let n = 5;
+// let string = "";
 
-for (let i = 0; i < n; i++) {
+// for (let i = 0; i < n; i++) {
   
-  for (let k = 0; k < n - i; k++) {
-    string += "*";
-  }
-}
-console.log(string);
+//   for (let k = 0; k < n - i; k++) {
+//     string += "*";
+//   }
+// }
+// console.log(string);
+
+// var greetings ="Hello";
+// function letGreet(){
+//   greetings="caio";
+// var greetings;
+// }
+// letGreet();
+// console.log(greetings);
+
+
+
+
